69440c5dc49f9c702c4cd762cce644e41f169250
Bryce Mecum <petridish@gmail.com>
2015-03-11 11:18:07 -0800

    Add pluginhost to util.cs

--- Util.cs
+++ Util.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 
+using Decal.Adapter;
+using Decal.Adapter.Wrappers;
+
 namespace TreeStats
 {
     public static class Util
     {
+        internal static PluginHost MyHost;
+
+        public static void Init(PluginHost host)
+        {
+            MyHost = host;
+        }
+
+        public static void Destroy()
+        {
+            MyHost = null;
+        }
+        
         public static void WriteToChat(string message)
         {
             try
             {
-                TreeStats.Host.Actions.AddChatText("[TreeStats] " + message, 1);
+                MyHost.Actions.AddChatText("[TreeStats] " + message, 1);
             }
             catch (Exception ex)
             {
