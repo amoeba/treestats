1e50d9e4e2ceae6d142ff8e23cb27aa31619ad06
Bryce Mecum <petridish@gmail.com>
2016-08-03 22:13:15 -0800

    Update the copyright to 2016

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KolthSoft")]
 [assembly: AssemblyProduct("TreeStats")]
-[assembly: AssemblyCopyright("Copyright © KolthSoft 2015")]
+[assembly: AssemblyCopyright("Copyright © KolthSoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
