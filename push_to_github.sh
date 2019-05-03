ad6e4577ceecf67f7b9686cd9b2266cbe7a3f32c
Bryce Mecum <petridish@gmail.com>
2019-05-02 22:39:21 -0800

    Fix bug in push script

--- push_to_github.sh
+++ push_to_github.sh
@@ -3,9 +3,9 @@
 set -e
 
 BRANCH="push"
-REMOTE="private"
+REMOTE="public"
 
 git branch $BRANCH
 git filter-branch --index-filter 'git rm --cached --ignore-unmatch Encryption.cs' -f $BRANCH
-git push --force -u $PRIVATE $BRANCH:master
-git branch -D BRANCH
+git push --force -u $REMOTE $BRANCH:master
+git branch -D $BRANCH
